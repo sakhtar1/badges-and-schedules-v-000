44316386b8435b712066e42c28515f7ba22ee5c2
Sharmina Akhtar <sakht318@gmail.com>
2017-11-12 05:48:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.each do |badge|
+     puts "Hello, my name is #{badge}."
+  end
+end
