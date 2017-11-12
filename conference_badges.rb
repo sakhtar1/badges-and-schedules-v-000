5308ec6d2a86e29a6e804949f216c4c2da90d69b
Sharmina Akhtar <sakht318@gmail.com>
2017-11-12 07:07:16 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,6 +17,6 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees) 
-  assign_rooms(attendees)
+  return batch_badge_creator(attendees) 
+  return assign_rooms(attendees)
 end
