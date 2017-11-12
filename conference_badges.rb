979ff69794660e1078d9b59feb9a34223ac11733
Sharmina Akhtar <sakht318@gmail.com>
2017-11-12 06:35:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index-1 do |attendee,index|
+  attendees.each.with_index-1 do |attendee,index|
     puts "Hello, #{attendee}! You'll be assigned to room #{index}!"
   end
 end
