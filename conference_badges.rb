7982c36da39ed855672f545a1839555c45ea3a0e
Sharmina Akhtar <sakht318@gmail.com>
2017-11-12 07:06:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,17 +6,16 @@ end
 
 def batch_badge_creator(attendees)
   attendees.collect do |badge|
-     print "Hello, my name is #{badge}."
+      "Hello, my name is #{badge}."
   end
 end
 
 def assign_rooms(attendees)
   attendees.collect.with_index(1) do |attendee,index|
-    print "Hello, #{attendee}! You'll be assigned to room #{index}!"
+    "Hello, #{attendee}! You'll be assigned to room #{index}!"
   end
 end
 
 def printer(attendees)
-  return batch_badge_creator(attendees) + assign_rooms(attendees)
-
+  batch_badge_creator(attendees) + assign_rooms(attendees)
 end
